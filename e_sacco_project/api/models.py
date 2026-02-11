923dc48f64e60788a120691a6ab31ea1a59b383f
sekibaalamark <markmajje256@gmail.com>
2026-02-11 21:14:33 +0300

    changed the user model to CustomUser

--- e_sacco_project/api/models.py
+++ e_sacco_project/api/models.py
@@ -2,16 +2,21 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class CustomeUser(AbstractUser):
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=15,unique=True)
     is_verified = models.BooleanField(default=False)
     username = models.CharField(max_length=30,unique=True)
-    role = models.CharField(max_length=15,unique=True)
-    
+    role = models.CharField(max_length=15,unique=True)    
 
     def __str__(self):
-        return self.username
+        return self.username 
+    
+
+
+    
+
+
     
 
 
