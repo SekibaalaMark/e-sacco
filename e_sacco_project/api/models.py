bf6d2fd996c8a54c2c61a237f3fb77eb1b71ae21
sekibaalamark <markmajje256@gmail.com>
2026-02-11 21:03:08 +0300

    added some unique fields on the user model

--- e_sacco_project/api/models.py
+++ e_sacco_project/api/models.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True)
+    phone_number = models.CharField(max_length=15)
     is_verified = models.BooleanField(default=False)
+    username = models.CharField(max_length=30,unique=True)
+    role = models.CharField(max_length=15,unique=True)
+    
 
     def __str__(self):
         return self.username
+    
+
+
