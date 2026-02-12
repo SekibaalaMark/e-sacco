94256e3f7b1fd150ca90844c006e8200b3fe8e05
sekibaalamark <markmajje256@gmail.com>
2026-02-12 21:05:05 +0300

    created roles options for user model

--- e_sacco_project/api/models.py
+++ e_sacco_project/api/models.py
@@ -3,11 +3,16 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('ordinary', 'Ordinary'),
+        ('treasurer', 'Treasurer'),
+    ]
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15,unique=True)
     is_verified = models.BooleanField(default=False)
     username = models.CharField(max_length=30,unique=True)
-    role = models.CharField(max_length=15)    
+    role = models.CharField(max_length=15,choices=ROLE_CHOICES,default='ordinary')    
 
     def __str__(self):
         return self.username 
