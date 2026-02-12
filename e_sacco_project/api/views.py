897a6e615b2e428b4cb48562962c3fb6b06ada64
sekibaalamark <markmajje256@gmail.com>
2026-02-12 21:40:31 +0300

    created the register API view

--- e_sacco_project/api/views.py
+++ e_sacco_project/api/views.py
@@ -3,3 +3,23 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
 
+
+class RegisterAPIView(APIView):
+    
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {
+                    "message": "User registered succesfully",
+                    "user":{
+                        "username": user.username,
+                        "email": user.email,
+                        "phone_number": user.phone_number,
+                        "role": user.role
+                    }
+                },status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
