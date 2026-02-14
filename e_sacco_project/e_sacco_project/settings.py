58f718f07a2fe0d473f506ca94d123d843e70be0
sekibaalamark <markmajje256@gmail.com>
2026-02-14 12:52:47 +0300

    re assigned the expiry time of the JWT tokens

--- e_sacco_project/e_sacco_project/settings.py
+++ e_sacco_project/e_sacco_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,3 +138,12 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=600),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
