1602040b61a3aeb05d03d05f001a8da0c7bae746
sekibaalamark <markmajje256@gmail.com>
2026-02-14 12:29:10 +0300

    created a base persmission for Treasurer User Types

--- e_sacco_project/api/permissions.py
+++ e_sacco_project/api/permissions.py
@@ -3,3 +3,9 @@ from rest_framework.permissions import BasePermission
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.groups.filter(name="Admin").exists()
+    
+
+
+class IsTreasurer(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.groups.filter(name="Treasurer").exists()
