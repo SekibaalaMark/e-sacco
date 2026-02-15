dbed86051a0e1e0d7b58610f42fd4a63e64dd98e
sekibaalamark <markmajje256@gmail.com>
2026-02-15 12:49:09 +0300

    added the verify email api to the urls.py app

--- e_sacco_project/api/urls.py
+++ e_sacco_project/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterAPIView
+from .views import *
 
 
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
+    path('verify-email/<str:uidb64>/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
 ]
