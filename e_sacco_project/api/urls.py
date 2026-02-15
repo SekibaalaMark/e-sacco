5953c52efead5a5a3e6c237a3e0fab27e8245134
sekibaalamark <markmajje256@gmail.com>
2026-02-15 13:23:08 +0300

    registered the resend verification view to the urls

--- e_sacco_project/api/urls.py
+++ e_sacco_project/api/urls.py
@@ -7,4 +7,5 @@ from .views import *
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('verify-email/<str:uidb64>/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
+    path('resend-verification/', ResendVerificationView.as_view(), name='resend-verification'),
 ]
