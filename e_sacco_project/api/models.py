d3de7a2d599ec8dbf53844efb9d4bccfb907ef83
sekibaalamark <markmajje256@gmail.com>
2026-02-16 09:24:11 +0300

    overrid the save method on custom user object such that a super user is verified automatically

--- e_sacco_project/api/models.py
+++ e_sacco_project/api/models.py
@@ -6,7 +6,13 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15,unique=True)
     is_verified = models.BooleanField(default=False)
-    username = models.CharField(max_length=30,unique=True)   
+    username = models.CharField(max_length=30,unique=True)
+
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.is_verified = True
+        super().save(*args, **kwargs)   
 
     def __str__(self):
         return self.username 
