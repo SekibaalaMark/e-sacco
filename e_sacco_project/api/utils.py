8a6184a28590489d6367a50cc8b1b731f9b3823e
sekibaalamark <markmajje256@gmail.com>
2026-02-16 08:19:42 +0300

    new migrations

--- e_sacco_project/api/utils.py
+++ e_sacco_project/api/utils.py
@@ -2,6 +2,10 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 
+from django.core.mail import send_mail
+from django.conf import settings
+from django.urls import reverse
+
 def generate_verification_token(user):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
@@ -9,13 +13,6 @@ def generate_verification_token(user):
 
 
 
-
-
-
-
-from django.core.mail import send_mail
-from django.conf import settings
-from django.urls import reverse
 
 def send_verification_email(user, request):
     # 1. Generate the UID and Token using your existing logic
