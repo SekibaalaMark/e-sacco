2c233c6d7a9b91ba6173d29a8c538978fc16c1d4
sekibaalamark <markmajje256@gmail.com>
2026-02-16 09:12:31 +0300

    added a logout serializer that blacklists a refresh token

--- e_sacco_project/api/serializers.py
+++ e_sacco_project/api/serializers.py
@@ -83,4 +83,20 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 
 
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+        try:
+            token = RefreshToken(self.token)
+            token.blacklist()
+        except Exception:
+            raise serializers.ValidationError("Invalid or expired token")
+
 
